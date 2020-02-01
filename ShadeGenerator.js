7c1a5d002ba0abfc1a1c41437512c31eabe99db6
pasha <pashap9990@gmail.com>
2020-02-01 17:32:00 +0330

    mius values fixed bug

--- ShadeGenerator.js
+++ ShadeGenerator.js
@@ -1,21 +1,32 @@
-const mix = function mix(color_1,color_2 = [0,0,0],width = 0.5){
+const mix = function mix(color_1, color_2 = [0, 0, 0], width = 0.5) {
     let f = color_1.map((x) => {
-        return width * x;
-    },[]);
+        let value = width * x;
+        if (value < 0)
+            return 0;
+        else if (value > 255)
+            return 255;
+        else
+            return value;
+    }, []);
 
     let g = color_2.map((x) => {
-        return (1 - width) * x;
+        let value = (1 - width) * x;
+        if (value < 0)
+            return 0;
+        else if (value > 255)
+            return 255;
+        else
+            return value;
     });
 
 
-    let h = f.map((value,key) => {
-        return value + g[key];
+    let h = f.map((value, key) => {
+        return Math.round(value + g[key]);
     });
     return h;
 };
 
-const shade= function shade(color, weight = 0.5)
-{
+const shade = function shade(color, weight = 0.5) {
     let t = color;
 
     let u = mix(t, [0, 0, 0], weight);
@@ -24,18 +35,18 @@ const shade= function shade(color, weight = 0.5)
 };
 
 
-const tint = function tine(color , wieght){
+const tint = function tine(color, wieght) {
     let t = color;
-    
-    let u = mix(t,[255,255,255],wieght);
+
+    let u = mix(t, [255, 255, 255], wieght);
 
     return u;
 };
 
-const tone = function tone(color,wieght){
+const tone = function tone(color, wieght) {
     let t = color;
-    
-    let u = mix(t,[128,128,128],wieght);
+
+    let u = mix(t, [128, 128, 128], wieght);
 
     return u;
 };
